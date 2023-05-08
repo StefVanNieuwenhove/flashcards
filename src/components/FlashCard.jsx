19929db4ed91f3576f7e54aab0cf41c13313cd17
Stef Van Nieuwenhove <stef.vannieuwenhove@telenet.be>
2023-05-08 14:17:20 +0200

    resize height of card

--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -5,16 +5,16 @@ const FlashCard = ({ title, description }) => {
   const [hidden, setHidden] = useState(false);
   return (
   
-    <Card variant='outlined' sx={{ borderColor: 'lightgray'}}>
-      <CardHeader title={title} />
+    <Card variant='outlined' sx={{ borderColor: 'lightgray', }}>
+      <CardHeader title={title} sx={{textDecoration: 'underline'}} />
       <Box sx={{
         display: 'flex',
-        height: '100px',
+        height: '150px',
         overflow: 'auto',
         width: '100%'
       }}>
       {hidden && (
-        <CardContent sx={{ }}>
+        <CardContent >
           <Typography  gutterBottom variant="h6" component="div">
             {description}
           </Typography>
